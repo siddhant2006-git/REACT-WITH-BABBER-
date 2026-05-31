f456722e5571f3549abf6cabb9d2e3bc7550a447
siddhant-2006 <siddhantbhatnagar11@gmail.com>
2026-05-31 23:27:58 +0530

    movie app

--- entertainment-hub/src/App.js
+++ entertainment-hub/src/App.js
@@ -1,12 +1,19 @@
 
 import './App.css';
-import Header from './components/Header/MainNav';
+import Header from './components/Header/Header';
+import FooterNavigation from './components/Header/MainNav';
+
+
+
 
 function App() {
   return (
     <>
       <div className="App">
         <Header />
+        <FooterNavigation/>
+      
+        
       </div>
     </>
   );
--- entertainment-hub/src/components/Header/MainNav.js
+++ entertainment-hub/src/components/Header/MainNav.js
@@ -1,13 +1,19 @@
 import * as React from "react";
+
+
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import PolicyIcon from "@mui/icons-material/Policy";
 
-import FolderIcon from "@mui/icons-material/Folder";
-import RestoreIcon from "@mui/icons-material/Restore";
+
+
+
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
+import FolderIcon from "@mui/icons-material/Folder";
 
-export default function LabelBottomNavigation() {
+export default function FooterNavigation() {
   const [value, setValue] = React.useState("recents");
 
   const handleChange = (event, newValue) => {
@@ -15,30 +21,50 @@ export default function LabelBottomNavigation() {
   };
 
   return (
-    <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
-      <BottomNavigationAction
-        label="Recents"
-        value="recents"
-        icon={<RestoreIcon />}
-      />
+    <>
+      <footer
+        style={{
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          backgroundColor: "white",
+          boxShadow: "0 -2px 10px rgba(0,0,0,0.1)",
+          zIndex: 1000,
+        }}
+      >
+        <BottomNavigation
+          sx={{ width: 500 }}
+          value={value}
+          onChange={handleChange}
+        >
+          <BottomNavigationAction
+            label="Tranding"
+            value="Tranding"
+            icon={<TrendingUpIcon />}
+          />
 
-      <BottomNavigationAction
-        label="Favorites"
-        value="favorites"
-        icon={<FavoriteIcon />}
-      />
+          <BottomNavigationAction
+            label="Favorites"
+            value="favorites"
+            icon={<FavoriteIcon />}
+          />
 
-      <BottomNavigationAction
-        label="Nearby"
-        value="nearby"
-        icon={<LocationOnIcon />}
-      />
+          <BottomNavigationAction
+            label="Nearby"
+            value="nearby"
+            icon={<LocationOnIcon />}
+          />
 
-      <BottomNavigationAction
-        label="Folder"
-        value="folder"
-        icon={<FolderIcon />}
-      />
-    </BottomNavigation>
+          <BottomNavigationAction
+            label="Folder"
+            value="folder"
+            icon={<FolderIcon />}
+          />
+        </BottomNavigation>
+      </footer>
+    </>
   );
 }
